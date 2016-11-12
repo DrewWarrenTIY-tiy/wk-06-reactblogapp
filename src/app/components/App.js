224936222420fda04847c3d66d0fb2b908765f12
DrewWarrenTIY <drewwarrentiy@gmail.com>
2016-11-12 14:19:33 -0700

    Chore(core) Move state of App component to Body Component
    Header and Footer didn't need to 'see' the Blog functionality.
    Moved functionality 'down a level' to keep blog functions
    localized in blog components.

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -6,47 +6,16 @@ import Header from './Header';
 import Body from './Body';
 import Footer from './Footer';
 
-import blogData from './blogposts.json';
-
 import container from './App.css';
 
-let monthArr = [];
-for (let i = 0; i < blogData.length; i++) {
-  if (monthArr.includes(blogData[i].date.month) != true)
-  monthArr.push(blogData[i].date.month);
-}
-
-let tagArr = [];
-for (let i = 0; i < blogData.length; i++) {
-  for (let j = 0; j < blogData[i].tags.length; j++) {
-    if (tagArr.includes(blogData[i].tags[j]) != true)
-    tagArr.push(blogData[i].tags[j]);
-  }
-}
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: blogData,
-      monthArr: monthArr,
-      tagArr: tagArr
-    }
-  };
-
-  clickadoodle(pizza) {
-    console.log(pizza);
-  }
-
   render () {
     return (
       <div className='container'>
         <Header />
         <div className='content'>
-          <Body
-            clickadoodle={this.clickadoodle.bind(this)} data={this.state.data}
-            monthArr={this.state.monthArr}
-            tagArr={this.state.tagArr}/>
+          <Body />
         </div>
         <Footer />
       </div>
--- src/app/components/Body.js
+++ src/app/components/Body.js
@@ -7,16 +7,45 @@ import Sidebar from './Body/Sidebar';
 
 import body from './Body.css';
 
+import blogData from './blogposts.json';
+
+let monthArr = [];
+for (let i = 0; i < blogData.length; i++) {
+  if (monthArr.includes(blogData[i].date.month) != true)
+  monthArr.push(blogData[i].date.month);
+}
+
+let tagArr = [];
+for (let i = 0; i < blogData.length; i++) {
+  for (let j = 0; j < blogData[i].tags.length; j++) {
+    if (tagArr.includes(blogData[i].tags[j]) != true)
+    tagArr.push(blogData[i].tags[j]);
+  }
+}
+
+
 export default class Body extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: blogData,
+      monthArr: monthArr,
+      tagArr: tagArr
+    }
+  };
+
+  clickadoodle(pizza) {
+    console.log(pizza);
+  };
 
   render () {
     return(
       <main className='body'>
-      <Main data={this.props.data} />
+      <Main data={this.state.data} />
       <Sidebar
-        clickadoodle={this.props.clickadoodle} data={this.props.data}
-        monthArr={this.props.monthArr}
-        tagArr={this.props.tagArr} />
+        clickadoodle={this.clickadoodle.bind(this)} data={this.state.data}
+        monthArr={this.state.monthArr}
+        tagArr={this.state.tagArr} />
       </main>
     );
   }
